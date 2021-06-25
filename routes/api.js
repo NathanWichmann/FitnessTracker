0e50e6ae93ff136f45dfb684e7d1fcadafe6f0da
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-06-25 16:01:47 -0400

    deploying

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require('../models/workout.js')
+const Workout = require('../Models/Workout.js')
 
 
 router.get("/api/workouts", (req, res) => {
