c805b3d4bdc1b97448e077807daabeb64ab6af09
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-06-25 16:36:41 -0400

    updates

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path');
-const compression = require('compression')
+const path = require("path");
+const compression = require("compression");
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,18 +16,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
-
-
-
-app.use(compression())
-
-
- app.use(require("./routes/api.js"));
- app.use(require("./routes/view.js"));
-
-
+app.use(compression());
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`'Server listening on: http://localhost:${PORT}`);
